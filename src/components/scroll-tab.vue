75ea9be819e97714c17be51af730114dc0312f4a
kinvirgo <jiangjinyu@ijovo.com>
2022-03-31 16:41:52 +0800

    update

--- src/components/scroll-tab.vue
+++ src/components/scroll-tab.vue
@@ -1,25 +1,110 @@
 <template>
     <div class="scroll-tab">
-        <div class="scroll-tab__box">
-            <ul class="tab">
-                <li class="tab-item"></li>
-                <li class="tab-item"></li>
-                <li class="tab-item"></li>
-            </ul>
+        <div class="scroll-tab__wrapper" ref="wrapperRef">
+            <div
+                class="scroll-tab__wrapper--box"
+                ref="boxRef"
+                :style="transform">
+                <div class="tab-item">1</div>
+                <div class="tab-item">2</div>
+                <div class="tab-item">3</div>
+                <div class="tab-item">4</div>
+                <div class="tab-item">5</div>
+            </div>
         </div>
         <div class="scroll-tab__control">
-            <button class="control-left">◀</button>
-            <button class="control-right">▶</button>
-            <button class="control-set">⚙</button>
+            <button
+                class="scroll-tab__control--left"
+                @click="doLeft">
+                ◀
+            </button>
+            <button
+                class="scroll-tab__control--right"
+                @click="doRight">
+                ▶
+            </button>
+            <button
+                class="scroll-tab__control--set"
+                @click="doSet">
+                ⚙
+            </button>
         </div>
     </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-    setup(props, context) {
-        return {}
-    },
-})
+    import { computed } from '@vue/reactivity'
+    import { defineComponent, reactive, toRefs } from 'vue'
+    export default defineComponent({
+        setup(props, context) {
+            const state = reactive({
+                wrapperRef: null,
+                boxRef: null,
+                duration: 250,
+                translateX: 0,
+                timingFunction: 'ease-in-out',
+            })
+
+            const transform = computed(() => {
+                let {
+                    duration,
+                    translateX,
+                    timingFunction,
+                } = state
+
+                return {
+                    'transform': `translate3d(${translateX}px, 0, 0)`,
+                    '-webkit-transform': `translate3d(${translateX}px, 0, 0)`,
+                    'transition': duration
+                        ? `transform ${duration}ms ${timingFunction}`
+                        : null,
+                }
+            })
+
+            function doLeft() {}
+            function doRight() {}
+            function doSet() {}
+
+            return {
+                // state
+                ...toRefs(state),
+                transform,
+
+                // event
+                doLeft,
+                doRight,
+                doSet,
+            }
+        },
+    })
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+    .scroll-tab {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        &__wrapper {
+            flex: 1;
+            overflow: hidden;
+
+            &--box {
+                min-width: 100%;
+                background-color: red;
+                white-space: nowrap;
+
+                .tab-item {
+                    display: inline-block;
+                    width: 120px;
+                    height: 44px;
+                    background-color: blue;
+                    vertical-align: middle;
+                    color: #fff;
+                }
+            }
+        }
+
+        &__control {
+            flex-shrink: 0;
+        }
+    }
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -3,22 +3,37 @@
         <scroll-tab />
     </div>
 </template>
-<script lang='ts'>
-import { defineComponent } from 'vue';
-import ScrollTab from "@/components/scroll-tab.vue"
-export default defineComponent({
-    components: {
-        ScrollTab
-    },
-    setup(props, context) {
-        return {}
-    }
-});
-</script>
-<style lang='scss'>
+<script lang="ts">
+    import { defineComponent, reactive, toRefs } from 'vue'
+    import ScrollTab from '@/components/scroll-tab.vue'
+    export default defineComponent({
+        components: {
+            ScrollTab,
+        },
+        setup(props, context) {
+            const state = reactive({
+                columns: [
+                    { name: '1' },
+                    { name: '2' },
+                    { name: '3' },
+                    { name: '4' },
+                    { name: '5' },
+                    { name: '6' },
+                    { name: '7' },
+                ],
+            })
 
-.tab-wrapper{
-    width: 80%;
-    margin: 0 auto;
-}
+            return {
+                ...toRefs(state),
+            }
+        },
+    })
+</script>
+<style lang="scss">
+    .tab-wrapper {
+        width: 80%;
+        margin: 50vh auto 0;
+
+        background: #f5f5f5;
+    }
 </style>
