c6dbf8238f856483ab0ca4010954dd04936596e6
kinvirgo <jiangjinyu@ijovo.com>
2022-04-01 13:44:27 +0800

    update

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,14 +1,22 @@
 <template>
     <div class="tab-wrapper">
-        <scroll-tab :columns="tabs" v-model="selectIndex" />
+        <scroll-tab
+            :columns="tabs"
+            v-model:select-index="selectIndex" />
+    </div>
+    <div class="tab-wrapper__action">
+        <button @click="doAdd">添加</button>
+        <button @click="doSub">删除</button>
     </div>
 </template>
 <script lang="ts">
     import { defineComponent, reactive, toRefs } from 'vue'
     import ScrollTab from '@/components/scroll-tab.vue'
+    import ScrollTabAlpha from '@/components/scroll-tab.alpha.vue'
     export default defineComponent({
         components: {
             ScrollTab,
+            ScrollTabAlpha,
         },
         setup(props, context) {
             const state = reactive({
@@ -21,8 +29,23 @@
                 selectIndex: 0,
             })
 
+            function doAdd() {
+                state.tabs.push({
+                    name: state.tabs.length + 1,
+                })
+            }
+
+            function doSub() {
+                state.tabs.shift()
+            }
+
             return {
+                /* state */
                 ...toRefs(state),
+
+                /* event */
+                doAdd,
+                doSub,
             }
         },
     })
@@ -33,5 +56,10 @@
         margin: 50vh auto 0;
 
         background: #f5f5f5;
+
+        &__action {
+            padding: 10px;
+            text-align: center;
+        }
     }
 </style>
